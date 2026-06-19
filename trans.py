37f869751cee01abf66f6a99b43528cbd241d859
moujanehouda <moujanehouda22@gmail.com>
2026-06-19 14:29:03 +0200

    Update trans.py

--- trans.py
+++ trans.py
@@ -15,11 +15,11 @@ def transformer():
     os.makedirs("HTML", exist_ok=True)
     with open("HTML/taches.html", "wb") as fichier_sortie:
         fichier_sortie.write(etree.tostring(document_html, pretty_print=True, method="html"))
-    html_abs = os.path.abspath("HTML/taches.html")
+    html = os.path.abspath("HTML/taches.html")
     print("Transformation reussie!")
     ouvrir = input("Ouvrir dans le navigateur ? (o/n) : ").strip().lower()
     if ouvrir == 'o':
-        webbrowser.open(f'file:///{html_abs}')
+        webbrowser.open(f'file:///{html}')
 
 
     
